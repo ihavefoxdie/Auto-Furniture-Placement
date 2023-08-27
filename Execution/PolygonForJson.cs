bc95917773301d1dc1346935b4c150db35a84763
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2023-08-27 19:59:43 +0300

    added: -class made specifically for .json serialization of IPolygon objects.

---
+++ Execution/PolygonForJson.cs
@@ -0,0 +1,50 @@
+﻿using Interfaces;
+namespace Execution;
+
+class PolygonForJson
+{
+    public int ID { get; set; }
+
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+
+    public decimal[] Center { get; set; }
+
+    public decimal[][] JaggedVertices { get; set; }
+
+    public string Name { get; set; }
+
+    public PolygonForJson(IPolygon item)
+    {
+        ID = item.ID;
+        Width = item.Width;
+        Height = item.Height;
+        Center = item.Center;
+        Name = item.Name;
+        JaggedVertices = new decimal[item.Vertices.GetLength(0)][];
+        for (int i = 0; i < JaggedVertices.Length; i++)
+        {
+            JaggedVertices[i] = new decimal[item.Vertices.GetLength(1)];
+            for (int j = 0; j < JaggedVertices[i].Length; j++)
+            {
+                JaggedVertices[i][j] = item.Vertices[i, j];
+            }
+        }
+    }
+
+    public PolygonForJson(int ID, int Width, int Height, decimal[] Center, decimal[][] JaggedVertices, string Name)
+    {
+        this.ID = ID;
+        this.Width = Width;
+        this.Height = Height;
+        this.Center = Center;
+        this.Name = Name;
+        this.JaggedVertices = JaggedVertices;
+    }
+
+    public PolygonForJson()
+    {
+
+    }
+}
--- Execution/Program.cs
+++ Execution/Program.cs
@@ -1,94 +1,96 @@
-﻿using Interfaces;
-using Furniture;
-using Rooms;
-using Vertex;
-using Rasterization;
+﻿using Execution;
 using FactoryMethod;
-using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
 
-namespace Testing
+namespace Testing;
+
+static class Program
 {
-    static class Program
+    static void Main()
     {
-        static void Main()
+        BedFactory bedFactory = new();
+        TableFactory tableFactory = new();
+        DoorFactory doorFactory = new();
+
+        List<PolygonForJson> rectangles = new List<PolygonForJson>();
+
+        rectangles.Add(new(bedFactory.GetFurniture()));
+
+        string jsonFile = JsonSerializer.Serialize(rectangles);
+        File.WriteAllText("rectangles.json", jsonFile);
+        string serializedPolygon = File.ReadAllText("rectangles.json");
+        Thread.Sleep(2000);
+        var deserialized = JsonSerializer.Deserialize<List<PolygonForJson>>(serializedPolygon);
+        Console.WriteLine(serializedPolygon);
+
+
+
+        /*int[] origRoomDim = new int[] { 160, 80 };
+        int[] roomDim = new int[] { 160, 80 };
+
+        List<GeneralFurniture> furnitures = new()
         {
-            BedFactory bedFactory = new();
-            TableFactory tableFactory = new();
-            DoorFactory doorFactory = new();
+            bedFactory.GetFurniture(),
+            tableFactory.GetFurniture()
 
-            List<IPolygon> rectangles = new List<IPolygon>();
-
-
-
-            rectangles.Add(bedFactory.GetFurniture());
-
-            /*int[] origRoomDim = new int[] { 160, 80 };
-            int[] roomDim = new int[] { 160, 80 };
-
-            List<GeneralFurniture> furnitures = new()
-            {
-                bedFactory.GetFurniture(),
-                tableFactory.GetFurniture()
-
-                //new GeneralFurniture(1, "bed", 40, 32, "livingRoom", false),
-                //new GeneralFurniture(2, "table", 40, 40, "kitchen", false, 2)
-            };
-            *//*for (int i = 0; i < furnitures.Count; i++)
-            {
-                furnitures[i].RotateVertex = VertexManipulator.VertexRotation;
-            }
-            furnitures[0].Move(0, 0);
-            furnitures[1].Move(10, 15);*//*
-            //furnitures[0].Rotate(45);
-
-            Console.WriteLine(furnitures[0].Vertices[0, 0] + " " + furnitures[0].Vertices[0, 1]);
-            Console.WriteLine(furnitures[0].Vertices[1, 0] + " " + furnitures[0].Vertices[1, 1]);
-            Console.WriteLine(furnitures[0].Vertices[2, 0] + " " + furnitures[0].Vertices[2, 1]);
-            Console.WriteLine(furnitures[0].Vertices[3, 0] + " " + furnitures[0].Vertices[3, 1]);
-            
-
-            Console.WriteLine(furnitures[0].Rotation);
-            int rotateFor = 360 - furnitures[0].Rotation;
-
-            //furnitures[0].Rotate(rotateFor);
-
-            Console.WriteLine(furnitures[0].Vertices[0, 0] + " " + furnitures[0].Vertices[0, 1]);
-            Console.WriteLine(furnitures[0].Vertices[1, 0] + " " + furnitures[0].Vertices[1, 1]);
-            Console.WriteLine(furnitures[0].Vertices[2, 0] + " " + furnitures[0].Vertices[2, 1]);
-            Console.WriteLine(furnitures[0].Vertices[3, 0] + " " + furnitures[0].Vertices[3, 1]);
-
-            List<GeneralFurniture> door = new()
-            {
-                doorFactory.GetFurniture()
-                //new(-1, "door", 15, 5, "ROOM", false, 0)
-            };
-            //door[0].Move(20, 0);
-
-            Room newRoom = new(40, 40, door, furnitures, _ = false)
-            {
-                DetermineCollision = VertexManipulator.DetermineCollision
-            };
-
-
-            Rasterizer.RasterizationMethod = LineDrawer.Line;
-
-            //newRoom.RasterizationMethod()
-
-            //newRoom.FurnitureArray[0].Rotate(31);
-            newRoom.RoomArray = Rasterizer.Rasterization(newRoom.FurnitureArray.ToList<IPolygon>(), newRoom.ContainerWidth, newRoom.ContainerWidth);
-
-            
-            LineDrawer.Print(newRoom.RoomArray);
-
-
-
-
-
-
-            Console.WriteLine(newRoom.Collision(newRoom.FurnitureArray[0], newRoom.FurnitureArray[1]));
-            newRoom.PenaltyEvaluation();
-            Console.WriteLine(newRoom.FurnitureArray[0].IsOutOfBounds); Console.WriteLine(newRoom.FurnitureArray[1].IsOutOfBounds);
-            Console.WriteLine(newRoom.Penalty);*/
+            //new GeneralFurniture(1, "bed", 40, 32, "livingRoom", false),
+            //new GeneralFurniture(2, "table", 40, 40, "kitchen", false, 2)
+        };
+        *//*for (int i = 0; i < furnitures.Count; i++)
+        {
+            furnitures[i].RotateVertex = VertexManipulator.VertexRotation;
         }
+        furnitures[0].Move(0, 0);
+        furnitures[1].Move(10, 15);*//*
+        //furnitures[0].Rotate(45);
+
+        Console.WriteLine(furnitures[0].Vertices[0, 0] + " " + furnitures[0].Vertices[0, 1]);
+        Console.WriteLine(furnitures[0].Vertices[1, 0] + " " + furnitures[0].Vertices[1, 1]);
+        Console.WriteLine(furnitures[0].Vertices[2, 0] + " " + furnitures[0].Vertices[2, 1]);
+        Console.WriteLine(furnitures[0].Vertices[3, 0] + " " + furnitures[0].Vertices[3, 1]);
+        
+
+        Console.WriteLine(furnitures[0].Rotation);
+        int rotateFor = 360 - furnitures[0].Rotation;
+
+        //furnitures[0].Rotate(rotateFor);
+
+        Console.WriteLine(furnitures[0].Vertices[0, 0] + " " + furnitures[0].Vertices[0, 1]);
+        Console.WriteLine(furnitures[0].Vertices[1, 0] + " " + furnitures[0].Vertices[1, 1]);
+        Console.WriteLine(furnitures[0].Vertices[2, 0] + " " + furnitures[0].Vertices[2, 1]);
+        Console.WriteLine(furnitures[0].Vertices[3, 0] + " " + furnitures[0].Vertices[3, 1]);
+
+        List<GeneralFurniture> door = new()
+        {
+            doorFactory.GetFurniture()
+            //new(-1, "door", 15, 5, "ROOM", false, 0)
+        };
+        //door[0].Move(20, 0);
+
+        Room newRoom = new(40, 40, door, furnitures, _ = false)
+        {
+            DetermineCollision = VertexManipulator.DetermineCollision
+        };
+
+
+        Rasterizer.RasterizationMethod = LineDrawer.Line;
+
+        //newRoom.RasterizationMethod()
+
+        //newRoom.FurnitureArray[0].Rotate(31);
+        newRoom.RoomArray = Rasterizer.Rasterization(newRoom.FurnitureArray.ToList<IPolygon>(), newRoom.ContainerWidth, newRoom.ContainerWidth);
+
+        
+        LineDrawer.Print(newRoom.RoomArray);
+
+
+
+
+
+
+        Console.WriteLine(newRoom.Collision(newRoom.FurnitureArray[0], newRoom.FurnitureArray[1]));
+        newRoom.PenaltyEvaluation();
+        Console.WriteLine(newRoom.FurnitureArray[0].IsOutOfBounds); Console.WriteLine(newRoom.FurnitureArray[1].IsOutOfBounds);
+        Console.WriteLine(newRoom.Penalty);*/
     }
 }
