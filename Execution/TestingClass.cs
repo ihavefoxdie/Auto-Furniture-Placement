223cf333ca226a1dbedaef0a97f888c648f78ec2
Gusiha <TUGARIN@TUGARIN>
2023-09-10 17:07:03 +0300

    test : add new class for testing

---
+++ Execution/TestingClass.cs
@@ -0,0 +1,32 @@
+﻿using FactoryMethod;
+using RoomClass.Zones;
+using Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execution
+{
+    internal class TestingClass
+    {
+        private FurnitureFactory _furnitureFactory { get; set; }
+        private Room _room { get; set; }
+
+        public void AnnealingTesting()
+        {
+
+
+
+        }
+
+        public void GeneticTesting()
+        {
+            
+
+
+        }
+
+    }
+}
